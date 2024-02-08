a05f65b8b62a43cdd593070b62a66f40bc987857
rupali123r <rupali3530rajput@gmail.com>
2024-02-08 15:28:45 +0530

    first commit

---
+++ src/pages/catagory-list.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState, } from "react";
+import { BrowserRouter as Router, Link, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
+
+function CatagoryList() {
+
+  useEffect(() => {
+    categoryDetail()
+  }, []);
+
+  const { cateurl } = useParams()
+  const [category, setcategory] = useState([]);
+
+
+  async function categoryDetail() {
+
+    try {
+      const categoryData = {
+        pageurl: cateurl
+      };
+
+      const response = await axios.post('https://bhopal.city/api/web/v1/category.php', categoryData);
+      //  console.log("hello",JSON.stringify(response.data));
+      setcategory(response.data.category);
+      // console.log("category",response.data )
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  return (
+    <>
+      {/* {cateurl} */}
+      <div className="container-fluid mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="list-card-page">
+                <h5>Search near by</h5>
+                <ul className="list-page">
+                  {category?.map((key) => {
+                    // console.log("key", key.pageurl)
+                    return (
+                      <li><Link to={key.pageurl}><FontAwesomeIcon icon={faAnglesRight} />  {key.categoryname}</Link></li>
+                    )
+                  })}
+
+               
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-4"></div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+export default CatagoryList;
---
+++ src/pages/list.js
@@ -0,0 +1,20 @@
+function List() {
+  return (
+    <>
+
+     
+      this is list page
+      <div class="">
+        <li class="dropdown " type="" data-bs-toggle="dropdown" aria-expanded="false">
+          myprofile
+        </li>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+export default List;
